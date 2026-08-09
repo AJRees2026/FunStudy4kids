78269830e5ff3b8af6a8bb38a9f5113d12efeb03
AJRees2026 <bochordum@gmail.com>
2026-08-09 21:47:17 +0800

    Add files via upload

---
+++ gemini-code-1786283183549.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Launch StudyPulse Kids with gender themes"
+git branch -M main
+git remote add origin https://github.com/YOUR_GITHUB_USERNAME/studypulse-kids.git
+git push -u origin main
