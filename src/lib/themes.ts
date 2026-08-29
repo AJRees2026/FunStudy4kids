6e97a0c83c60f7dc0401f0668bf382d34b17f4fa
Jackie & Andrew <bochordum@gmail.com>
2026-08-29 13:26:03 +0800

    Updated themes.ts

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -24,7 +24,7 @@ export const spaceTheme: Theme = {
   textPrimary: 'text-white',
   textSecondary: 'text-red-300',
   textMuted: 'text-[#dcdcdc]',
-  accent: 'text-black',
+  accent: 'text-[#ef684d]',
   accentLight: 'bg-[#ef684d]/20',
   accentGradient: 'from-[#6f5bd3] to-[#ef684d]',
   starLabel: 'Fuel Cells',
