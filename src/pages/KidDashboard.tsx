ace077e30a880a1756b1ed6b8150cbda67854cb2
Jackie & Andrew <bochordum@gmail.com>
2026-08-29 14:13:29 +0800

    Updated KidDashboard.tsx

--- src/pages/KidDashboard.tsx
+++ src/pages/KidDashboard.tsx
@@ -422,7 +422,7 @@ export default function KidDashboard({ child, onSwitchProfile }: Props) {
                   <Ruler className="w-6 h-6 text-white" />
                 </div>
                 <h3 className={`font-display font-extrabold text-lg text-[#df5a00] mb-1`}>{t('howMuchIVeGrown')}</h3>
-                <p className={`text-xs font-semibold text-[#4169e1] leading-snug font-nunito`}>{t('growthDesc')}</p>
+                <p className={`text-s font-semibold text-[#4169e1] leading-snug font-nunito`}>{t('growthDesc')}</p>
                 <div className="hidden">
                   {t('growthChart')} <ChevronRight className="w-3 h-3" />
                 </div>
