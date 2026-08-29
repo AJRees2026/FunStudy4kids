b3d3387d6f31969a69330f3c0578f531259e9c8a
Jackie & Andrew <bochordum@gmail.com>
2026-08-29 13:40:28 +0800

    Updated KidDashboard.tsx

--- src/pages/KidDashboard.tsx
+++ src/pages/KidDashboard.tsx
@@ -361,7 +361,7 @@ export default function KidDashboard({ child, onSwitchProfile }: Props) {
                   <Smile className="w-6 h-6 text-white" />
                 </div>
                 <h3 className={`font-display font-extrabold text-lg text-[#df5a00] mb-1`}>{t('moodTracker')}</h3>
-                <p className={`text-xs font-semibold ${theme.textMuted} leading-snug`}>{t('moodTrackerDesc')}</p>
+                <p className={`text-xs font-semibold text-[#89cff0] leading-snug`}>{t('moodTrackerDesc')}</p>
                  <div className="hidden">
                   {t('moodTracker')} <ChevronRight className="w-3 h-3" />
                 </div>
