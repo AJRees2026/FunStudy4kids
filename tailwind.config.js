007bad9340587d30f2f826b3b7a5566a42fe4a2c
Jackie & Andrew <bochordum@gmail.com>
2026-08-29 13:57:13 +0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        display: ['Fredoka', 'Inter', 'sans-serif'],
+        display: ['"Nunito"', 'Inter', 'sans-serif'],
       },
       colors: {
         boy: {
